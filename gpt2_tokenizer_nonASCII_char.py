80695447e503c2aa5b6c1d49b68f4a62b05f90fd
huangxt39 <55071802+huangxt39@users.noreply.github.com>
2024-06-18 12:17:28 +0200

    Update gpt2_tokenizer_nonASCII_char.py

--- gpt2_tokenizer_nonASCII_char.py
+++ gpt2_tokenizer_nonASCII_char.py
@@ -22,6 +22,8 @@ print(bytearray([tokenizer.byte_decoder[c] for c in raw_str]))
 test_str = "Søren bor i Kirsebærhaven i København, han har besøgt Saarbrücken siste år."
 test_str = "Bien sûr, voici un texte aléatoire en français: et à trouver le trésor qui"
 test_str = "ü ÿ å û ß Å Æ bü bÿ bå bû bß bÅ bÆ"
+test_str = "This Beats Mycroft’s Mind."
+test_str = "青いから means"
 
 # method 1
 tokens = tokenizer.convert_ids_to_tokens(tokenizer(test_str)["input_ids"])
@@ -37,13 +39,26 @@ print("".join(new_decoding))
 def rearrange(tokens):
     # this changes the model prediction, it's just a temporary workaround
     tokens = tokens.copy()
-    for i in range(len(tokens)):
-        if tokens[i] == "ĠÃ" and (i < len(tokens)-1):
-            tokens[i] = "Ġ_"
-            tokens[i+1] = "Ã" + tokens[i+1]
-        elif tokens[i] == "Ã" and (i < len(tokens)-1):
-            tokens[i] = "_"
-            tokens[i+1] = "Ã" + tokens[i+1]
+    for i in range((len(tokens))):
+        decoded = bytearray([tokenizer.byte_decoder[c] for c in tokens[i]]).decode("utf-8", errors="replace") 
+        if "�" in decoded and (i < len(tokens)-1) and len(tokens[i]) < 4:
+            t = tokens[i].lstrip("Ġ")
+            tokens[i] = "Ġ_" if tokens[i].startswith("Ġ") else "_"
+            tokens[i+1] = t + tokens[i+1]
+
+    # for i in range(len(tokens)):
+    #     if tokens[i] == "ĠÃ" and (i < len(tokens)-1):
+    #         tokens[i] = "Ġ_"
+    #         tokens[i+1] = "Ã" + tokens[i+1]
+    #     elif tokens[i] == "Ã" and (i < len(tokens)-1):
+    #         tokens[i] = "_"
+    #         tokens[i+1] = "Ã" + tokens[i+1]
+    #     elif tokens[i] == "ĠâĢ" and (i < len(tokens)-1):
+    #         tokens[i] = "Ġ_"
+    #         tokens[i+1] = "âĢ" + tokens[i+1]
+    #     elif tokens[i] == "âĢ" and (i < len(tokens)-1):
+    #         tokens[i] = "_"
+    #         tokens[i+1] = "âĢ" + tokens[i+1]
         
     return tokens
 
